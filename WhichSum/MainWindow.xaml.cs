77c4d1e4483209368a82ba26b9642941236ee231
LeiYangGH <675686066@qq.com>
2021-01-13 16:44:27 +0800

    can get result

--- WhichSum/MainWindow.xaml.cs
+++ WhichSum/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Lib.Calculator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,43 @@ namespace WhichSum
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Regex regNums = new Regex(@"\d+", RegexOptions.Compiled);
+        ICalculator calculator = new RecursiveCaculator();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private List<long> ExtractNumbersFromTxt(string txt)
+        {
+            return regNums.Matches(txt).Select(x => Convert.ToInt64(x.Value)).ToList();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            txtResults.Text = "";
+            try
+            {
+                List<long> nums = ExtractNumbersFromTxt(this.txtNums.Text);
+                if (nums.Count() > 5)
+                {
+                    txtMsg.Text = "测试版最多支持5个数";
+                    return;
+                }
+                long sum = Convert.ToInt64(this.txtSum.Text.Trim());
+                foreach (List<long> arr in calculator.Calculate(nums, sum))
+                {
+                    txtResults.Text += string.Join('\t', arr);
+                    txtResults.Text += Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                txtMsg.Text = ex.Message;
+            }
+
         }
     }
 }
